72d9e954f4a41b0910b06be6169d108a8db98840
Nickolay Petrych <npetrych@alfabank.ru>
2024-04-19 16:42:39 +0100

    add WebDriverWait class

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,10 +1,12 @@
 import org.testng.annotations.Test;
 
 public class MyTest extends BaseTest {
+    public String expectURL = "https://google.com";
 
     @Test
     public void myTestJava() {
         driver.get("https://google.com");
-        System.out.println("Title is " + driver.getTitle());
+        System.out.println("URL matches expectValue " + expectURL.equals(driver.getCurrentUrl()));
+
     }
 }
